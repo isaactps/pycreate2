f6625dd023b73be470c58fb7c9d668bad9406a5b
Isaac Tan <isaac_tps@yahoo.com>
2019-05-20 08:50:28 -0400

    Support for TCP, UDP & mBot Ranger control

--- pycreate2/create2api.py
+++ pycreate2/create2api.py
@@ -12,8 +12,13 @@ from __future__ import division
 import struct  # there are 2 places that use this ... why?
 import time
 from pycreate2.packets import SensorPacketDecoder
+from pycreate2.packets import SensorPacketDecoder_0
+from pycreate2.packets import SensorPacketDecoder_101
+
 from pycreate2.createSerial import SerialCommandInterface
 from pycreate2.createMQTT import MQTTCommandInterface
+from pycreate2.createTCP import TCPCommandInterface
+from pycreate2.createUDP import UDPCommandInterface
 from pycreate2.createEcho import EchoCommandInterface
 from pycreate2.OI import OPCODES
 # from pycreate2.OI import calc_query_data_len
@@ -32,6 +37,13 @@ from pycreate2.OI import DRIVE
 # class Warning(Exception):
 # 	pass
 
+#Constants for iRobot Create 2 platform
+WHEEL_PERIMETER = 226.19    # wheel perimeter in mm
+WHEEL_COUNTS = 511.0        # encoder counts per full revolution
+DRIVE_PERIMETER = 738.2742  # 2*pi*115.4; 115.4 = Create2 centre to wheel centre
+CLIFF_THRES = 100           # threshold for cliff sensors to detect a cliff
+DRIVE_SPEED = 80            # suitable speed for driving straight
+TURN_SPEED = 70             # suitable speed for turning (on the spot)
 
 class Create2(object):
 	"""
@@ -39,43 +51,78 @@ class Create2(object):
 	This is the only class that outside scripts should be interacting with.
 	"""
 
-	#def __init__(self, port, baud=115200):
 	def __init__(self, config):
 		"""
 		Constructor, sets up class
 		- creates serial port
 		- creates decoder
 		"""
-                if config["transport"] == 'mqtt':
-                    self.SCI = MQTTCommandInterface()
-                    self.SCI.open(port)
-                elif config["transport"] == 'echo':
-                    self.SCI = EchoCommandInterface()
-                    self.SCI.open()
-                else:
-                    self.SCI = SerialCommandInterface()
-                    self.SCI.open(config["port"], config["baud"])
+		if config["transport"] == 'mqtt':
+			self.SCI = MQTTCommandInterface()
+			self.SCI.open(port)
+		elif config["transport"] == 'echo':
+			self.SCI = EchoCommandInterface()
+			self.SCI.open()
+		elif config["transport"] == 'tcp':
+			self.SCI = TCPCommandInterface()
+			self.SCI.open(config["server_ip_addr"])
+		elif config["transport"] == 'udp':
+			self.SCI = UDPCommandInterface()
+			self.SCI.open(config["server_ip_addr"],config["logfile_name"])
+		else:
+			self.SCI = SerialCommandInterface()
+			self.SCI.open(config["port"], config["baud"])
+
 		# self.decoder = SensorPacketDecoder()
 		self.decoder = None
 		self.sleep_timer = 0.5
 
+		if config["robot"] == 'Create2':
+			self.robot = 'Create2'
+		elif config["robot"] == 'Ranger':
+			self.robot = 'Ranger'
+
+	#def __init__(self, port, baud=115200):
+		"""
+		Constructor, sets up class
+		- creates serial port
+		- creates decoder
+		"""
+		"""
+		if 'mqtt' in port:
+			self.SCI = MQTTCommandInterface()
+			self.SCI.open(port)
+		elif 'tcp' in port:
+			self.SCI = TCPCommandInterface()
+			self.SCI.open(port)
+		else:
+			self.SCI = SerialCommandInterface()
+			self.SCI.open(port, baud)
+		# self.decoder = SensorPacketDecoder()
+		self.decoder = None
+		self.sleep_timer = 0.5
+		"""
+
 	def __del__(self):
 		"""Destructor, cleans up when class goes out of scope"""
-		# stop motors
-		self.drive_stop()
-		time.sleep(self.sleep_timer)
 
-		# turn off LEDs
-		self.led()
-		self.digit_led_ascii('    ')
-		time.sleep(0.1)
+		if self.robot == 'Create2':
+			# stop motors
+			self.drive_stop()
+			time.sleep(self.sleep_timer)
 
-		# close it down
-		# self.power()
-		# self.safe()  # this beeps every now and then, but doesn't seem to go off
-		time.sleep(0.1)
-		self.stop()  # power down, makes a low beep sound
-		time.sleep(0.1)
+			# turn off LEDs
+			self.led()
+			self.digit_led_ascii('    ')
+			time.sleep(0.1)
+
+			# close it down
+			# self.power()
+			# self.safe()  # this beeps every now and then, but doesn't seem to go off
+			time.sleep(0.1)
+			self.stop()  # power down, makes a low beep sound
+			time.sleep(0.1)
+
 		self.close()  # close serial port
 		time.sleep(0.1)
 
@@ -291,6 +338,124 @@ class Create2(object):
 		if stop:
 			self.drive_stop()
 
+	def turn_angle_v2(self, angle, speed=TURN_SPEED): #Alternative version to command the iRobot to rotate by a specified angle
+		"""
+		Uses the encoders to turn an angle in degrees. This is a best effort,
+		the results will not be perfect due to wheel slip and encoder errors.
+
+		CCW is positive
+		CW is negative
+		"""
+
+		arclength = (float(abs(angle))/360.0) * DRIVE_PERIMETER 
+		
+		#since we are only reading from left encoder in driveDistance()
+		if angle > 0:
+		    arclength *= -1
+		else:
+		    arclength = arclength
+		
+		#print("arc_length: " + str(arclength))
+		if angle < 0:
+		    self.drive_distance_v2(speed, (-1)*speed, arclength, 0) 
+		else:
+		    self.drive_distance_v2((-1)*speed, speed, arclength, 0)
+
+		# stop Robot
+		self.drive_stop()
+
+		return 0
+
+	def drive_distance_v2(self, l_vel, r_vel, distance, if_straight): #Alternative version to command the iRobot to move by a specified distance
+		"""
+		Uses the encoders to drive forward or backwards in either straight or turning manner. This is a best effort,
+		the results will not be perfect due to wheel slip and encoder errors.
+		distance: mm
+		velocity: mm/sec, positive is forward, negative is backwards
+		"""
+		
+		#print("des_dis: " + str(distance))
+
+		count0 = self.getEncoderCountL()     #only use the left encoder
+		#print("count0: " + str(count0))
+
+		if if_straight == 1:
+		    if distance < 0:
+		        self.drive_direct(-r_vel, -r_vel) # right wheel velocity, left wheel velocity
+			#self.drive_straight(-r_vel)
+		    else:
+		        self.drive_direct(r_vel, r_vel)
+			#self.drive_straight(r_vel)
+		else:
+		    self.drive_direct(r_vel, l_vel)
+
+		count1 = self.getEncoderCountL()
+
+		#print("count1: " + str(count1))
+
+		cur_distance = ( ((count1-count0)/WHEEL_COUNTS)*WHEEL_PERIMETER ) 
+		#print("cur_dis: " + str(cur_distance))
+		prev_count1 = count1
+
+		#keep refreshing distance while robot drives
+		while abs(cur_distance) < abs(distance-1):
+			prev_count1 = count1
+			count1 = self.getEncoderCountL()
+			#print("count1: " + str(count1))
+
+			if (distance > 0) and (count1 < count0) and (abs(count1 - prev_count1) > 65000): # rollover from 65535 to 0 has occurred
+				count1 += 65535
+
+			elif (distance < 0) and (count1 > count0) and (abs(count1 - prev_count1) > 65000): # rollover from 0 to 65535 has occurred
+				count1 -= 65535	
+
+			cur_distance = ((count1-count0)/WHEEL_COUNTS)*WHEEL_PERIMETER 
+			#print("cur_dis: " + str(cur_distance))
+			time.sleep(0.015)
+
+		#print("cur_dis at End: " + str(cur_distance))
+		#print("des_dis at End: " + str(distance))
+
+		# stop Robot
+		self.drive_stop()
+
+		return 0
+
+	def turn_angle_Ranger(self, angle, speed=150):
+		"""
+		Uses the encoders to turn an angle in degrees. This is a best effort,
+		the results will not be perfect due to wheel slip and encoder errors.
+
+		CCW is positive
+		CW is negative
+		"""
+
+		speed = self.limit(speed, 0, 255)
+		data = struct.unpack('4B', struct.pack('>2h', speed, angle))
+		self.SCI.write(OPCODES.TURN_ANGLE_RANGER, data)
+
+	def drive_distance_Ranger(self, distance, speed=150):
+		"""
+		Uses the encoders to drive straight forward or backwards. This is a best effort,
+		the results will not be perfect due to wheel slip and encoder errors.
+			distance: inches, positive is forward, negative is backwards
+			speed: Ranger's Encoder motor PWM speed, max value is 255
+		"""
+			
+		speed = self.limit(speed, 0, 255)
+		data = struct.unpack('4B', struct.pack('>2h', speed, distance))
+		self.SCI.write(OPCODES.DRIVE_DIST_RANGER, data)
+
+
+	def line_follow_Ranger(self):
+		"""
+		Line following mode of mBot Ranger
+		Time of operating this mode is determined by main calling acript
+
+		"""
+
+		self.SCI.write(OPCODES.LINE_FOLLOW_RANGER)
+
 	# ------------------------ LED ----------------------------
 
 	def led(self, led_bits=0, power_color=0, power_intensity=0):
@@ -384,17 +549,144 @@ class Create2(object):
 		"""
 		return: a namedtuple
 
-		WARNING: now this only returns pkt 100, everything. And it is the default
-			packet reques now.
+		WARNING: now this only returns either pkt 0, 101 and 100, everything.
 		"""
 
 		opcode = OPCODES.SENSORS
+		
+		"""
 		cmd = (100,)
 		sensor_pkt_len = 80
 
 		self.SCI.write(opcode, cmd)
 		time.sleep(0.015)  # wait 15 msec
 		packet_byte_data = self.SCI.read(sensor_pkt_len)
+
 		sensors = SensorPacketDecoder(packet_byte_data)
+		"""
+
+		cmd = (0,)
+		sensor_pkt_len = 26
+
+		self.SCI.write(opcode, cmd)
+		time.sleep(0.015)  # wait 15 msec
+		packet_byte_data1 = self.SCI.read(sensor_pkt_len)
+
+		sensors = SensorPacketDecoder_0(packet_byte_data1)
+
+		"""
+		cmd = (101,)
+		sensor_pkt_len = 28
+
+		self.SCI.write(opcode, cmd)
+		time.sleep(0.015)  # wait 15 msec
+		packet_byte_data2 = self.SCI.read(sensor_pkt_len)
+		
+		sensors = SensorPacketDecoder_101(packet_byte_data2)
+		"""
 
 		return sensors
+
+	def getEncoderCountL(self):
+		"""function to grab current encoder counts on left wheel
+        	Parameters: None 
+        	return: encoder count on left wheel (-32767 to 32767)
+        	"""
+		opcode = OPCODES.SENSORS
+		cmd = (43,)
+		sensor_pkt_len = 2
+
+		self.SCI.write(opcode, cmd)
+		time.sleep(0.015)  # wait 15 msec
+		count_left = self.SCI.read(sensor_pkt_len)
+
+		left_count = (ord(count_left[0])<<8) | ord(count_left[1])
+
+		#print(left_count)
+
+		return left_count
+
+	def getEncoderCountR(self):
+		"""function to grab current encoder counts on right wheel
+        	Parameters: None
+        	return: encoder count on right wheel (-32767 to 32767)
+        	"""
+		opcode = OPCODES.SENSORS
+		cmd = (44,)
+		sensor_pkt_len = 2
+
+		self.SCI.write(opcode, cmd)
+		time.sleep(0.015)  # wait 15 msec
+		count_right = self.SCI.read(sensor_pkt_len)
+
+		right_count = (ord(count_right[0])<<8) | ord(count_right[1])
+
+		#print(right_count)
+
+		return right_count
+
+	def getEncoderCountLR(self):
+		"""function to grab current encoder counts on both left and right wheel simultaneously
+		Parameters: None 
+		return: encoder count on left wheel (-32767 to 32767)
+		"""
+		opcode = OPCODES.QUERY_LIST
+		cmd = (2,43,44,)
+		sensor_pkt_len = 4
+
+		self.SCI.write(opcode, cmd)
+		time.sleep(0.015)  # wait 15 msec
+		ret_bytes = self.SCI.read(sensor_pkt_len)
+
+		left_count = (ord(ret_bytes[0])<<8) | ord(ret_bytes[1])
+		right_count = (ord(ret_bytes[2])<<8) | ord(ret_bytes[3])
+
+		#print(left_count, right_count)
+
+		return left_count,right_count
+
+	def getDistance(self):
+		"""function to grab relative distance travelled in mm
+        	Parameters: None
+        	return: relative distance travelled in mm (-32767 to 32767)
+			+ve value for forward direction and -ve for reverse direction
+		"""
+		opcode = OPCODES.SENSORS
+		cmd = (19,)
+		sensor_pkt_len = 2
+
+		self.SCI.write(opcode, cmd)
+		time.sleep(0.015)  # wait 15 msec
+		dist_byte = self.SCI.read(sensor_pkt_len)
+
+		distance = (ord(dist_byte[0])<<8) | ord(dist_byte[1])
+
+		if distance > 32767:
+			distance = 0 - (65535 - distance)
+		#print(distance)
+
+		return distance
+
+	def getAngle(self):
+		"""function to grab relative angle turned in degrees
+        	Parameters: None
+        	return: relative angle turned in degrees (-32767 to 32767)
+			+ve value for CCW direction and -ve for CW direction
+		"""
+		opcode = OPCODES.SENSORS
+		cmd = (20,)
+		sensor_pkt_len = 2
+
+		self.SCI.write(opcode, cmd)
+		time.sleep(0.015)  # wait 15 msec
+		angle_byte = self.SCI.read(sensor_pkt_len)
+
+		angle = (ord(angle_byte[0])<<8) | ord(angle_byte[1])
+ 
+		if angle > 32767:
+			angle = 0 - (65535 - angle)
+
+		#print(angle)
+
+		return angle
+
